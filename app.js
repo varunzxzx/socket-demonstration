2b3896b65b76f72ec6a0eddd97073e401ab623f4
varunzxzx <varun995862@gmail.com>
2018-01-17 22:21:07 +0530

    change default html5 audio with howler.js

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/',express.static(path.join(__dirname, 'public')));
 app.use('/play',express.static(path.join(__dirname, 'public/play')));
 
--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -1,15 +1,21 @@
 var socket = io();
+var sound = new Howl({
+    src: ['/resonance.mp3'],
+    loop: true
+  });
 $(function() {
     socket.emit('user',"user")
     socket.on('count',function(data) {
         $('#user_count').text(data);
     });
     socket.on('play',function(data) {
-        var x = document.getElementById("myAudio");
-        x.play();
+        // var x = document.getElementById("myAudio");
+        // x.play();
+        sound.play();
     });
     socket.on('stop',function(data) {
-        var x = document.getElementById("myAudio");
-        x.pause();
+        // var x = document.getElementById("myAudio");
+        // x.pause();
+        sound.stop();
     });
 })
